6eec674cd0c3a8b90460d2422d53694d12c5b549
Andy Payne <physiphile@gmail.com>
2026-09-24 12:54:27 -0500

    Close remaining mobile HTTP connections during shutdown

--- lib/mobile/node.go
+++ lib/mobile/node.go
@@ -226,7 +226,11 @@ func (n *Node) Close() error {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	return n.server.Shutdown(ctx)
+	err := n.server.Shutdown(ctx)
+	if err != nil {
+		_ = n.server.Close()
+	}
+	return err
 }
 
 // Descriptor is private IPC. The capability is never returned by HTTP stats.
